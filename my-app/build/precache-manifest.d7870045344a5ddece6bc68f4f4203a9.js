a69829cab7a976dc7913a9b22b85256dab004b12
tzkhan1999 <63169081+tzkhan1999@users.noreply.github.com>
2020-04-04 21:02:24 -0400

    Create precache-manifest.d7870045344a5ddece6bc68f4f4203a9.js

---
+++ my-app/build/precache-manifest.d7870045344a5ddece6bc68f4f4203a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7175e0386976f716bc58a2b78cdbe6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "633719311bc10fd1e4ce",
+    "url": "/static/css/main.be40ea26.chunk.css"
+  },
+  {
+    "revision": "9035383f4305ddde4ce4",
+    "url": "/static/js/2.63073618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.63073618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633719311bc10fd1e4ce",
+    "url": "/static/js/main.909672c7.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
